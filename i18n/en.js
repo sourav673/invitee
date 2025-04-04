c9e6599b6d4596a7c73b09176c300f6ad59550f5
Sourav <sourav@Souravs-MacBook-Air.local>
2025-04-04 14:28:45 +0530

    update

---
+++ i18n/en.js
@@ -0,0 +1,15 @@
+Object.assign((typeof window === 'undefined' ? module.exports : window).i18n = (typeof window === 'undefined' ? module.exports : window).i18n || {}, {
+  en: {
+    TitleText: 'Connect on Delta Chat',
+    DescriptionText: 'Delta Chat is a messaging app that works over e-mail.',
+    FormBlurbText: 'Create a Delta Chat invite link',
+    StepCodeText: 'Open Delta Chat, then navigate to <b>QR code → Show QR Invite Code → Copy to Clipboard</b>',
+    StepPasteText: 'Paste into the following field:',
+    StepShareText: 'Share the generated link over any channel',
+    ShareLinkText: 'Open link',
+    JoinText: 'Say hello to',
+    JoinGroupText: 'Join',
+    ChatText: 'Open chat',
+    DownloadText: 'Download Delta Chat',
+  },
+});
---
+++ i18n/fr.js
@@ -0,0 +1,15 @@
+Object.assign((typeof window === 'undefined' ? module.exports : window).i18n = (typeof window === 'undefined' ? module.exports : window).i18n || {}, {
+  fr: {
+    TitleText: 'Connecte sur Delta Chat',
+    DescriptionText: 'Delta Chat est une application de messagerie qui fonctionne par e-mail.',
+    FormBlurbText: "Crée un lien d'invitation à Delta Chat",
+    StepCodeText: "Ouvre Delta Chat, navigue jusqu'à <b>QR code → Afficher le code d'invitation QR → Copier dans le presse-papiers </b>.",
+    StepPasteText: 'Colle dans le champ suivant :',
+    StepShareText: "Partage le lien généré sur n'importe quel canal",
+    ShareLinkText: 'Ouvre lien',
+    JoinText: 'Dit bonjour à',
+    JoinGroupText: 'Rejoine',
+    ChatText: 'Ouvrir discussion',
+    DownloadText: 'Télécharge Delta Chat',
+  },
+});
